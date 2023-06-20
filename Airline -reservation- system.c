b1fb4b4c9e7dd2b8bc5febfbf37ebfc48b39d2cb
Tanushree Borase <130696335+TanushreeBorase@users.noreply.github.com>
2023-06-20 21:22:57 +0530

    Update Airline -reservation- system.c

--- Airline -reservation- system.c
+++ Airline -reservation- system.c
@@ -4,7 +4,16 @@
 
 #define MAX_SEATS 10
 
-typedef struct {
+typedef struct 
+{
     char name[50];
     bool reserved;
 } Seat;
+
+
+Seat seats[MAX_SEATS];
+
+void initializeSeats() {
+    for (int i = 0; i < MAX_SEATS; i++) {
+        seats[i].reserved = false;
+    }
