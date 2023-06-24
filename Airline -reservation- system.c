593a0e8ab285d09b395c0f1ba3cb10cb1a884c82
Tanushree Borase <130696335+TanushreeBorase@users.noreply.github.com>
2023-06-24 10:32:13 +0530

    Update Airline -reservation- system.c

--- Airline -reservation- system.c
+++ Airline -reservation- system.c
@@ -56,7 +56,8 @@ void reserveSeat()
     }
 }    
 
-// The cancelReservation function prompts the user to enter a seat number and cancels the reservation if the seat is already reserved.
+// The cancelReservation function prompts the user to enter a seat number 
+// and cancels the reservation if the seat is already reserved.
 
 void cancelReservation() {
     int seatNumber;
@@ -64,6 +65,12 @@ void cancelReservation() {
     printf("Enter the seat number you want to cancel reservation: ");
     scanf("%d", &seatNumber);
 
+     if (seatNumber < 1 || seatNumber > MAX_SEATS) {
+        printf("Invalid seat number.\n");
+        return;
+    }
+
+    
 
 
 
